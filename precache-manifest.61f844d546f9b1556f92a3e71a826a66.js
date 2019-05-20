6c164fc9e47b51c517dc440e288d941b50166126
Thepsunan Limpaphayom <dhepsunan@hotmail.com>
2019-05-21 05:00:28 +0700

    Updates

---
+++ precache-manifest.61f844d546f9b1556f92a3e71a826a66.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b19e976073122bac44a542ed578e44c",
+    "url": "/stats_website/index.html"
+  },
+  {
+    "revision": "5badfd2e91bce13ffcfb",
+    "url": "/stats_website/static/css/main.9d548fa1.chunk.css"
+  },
+  {
+    "revision": "8b63b42ebc8c4b562c1c",
+    "url": "/stats_website/static/js/2.cf1a1d54.chunk.js"
+  },
+  {
+    "revision": "5badfd2e91bce13ffcfb",
+    "url": "/stats_website/static/js/main.998cd064.chunk.js"
+  },
+  {
+    "revision": "b3f76d094ea3d0485e3f",
+    "url": "/stats_website/static/js/runtime~main.d2034e86.js"
+  }
+]);
